5c1a93e8597892dccb160c002dbdf45c33d9a833
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-10-15 17:25:27 -0400

    Using classList

--- DOM/index.js
+++ DOM/index.js
@@ -8,6 +8,12 @@ document.getElementsByClassName("btn")[0].style.color = "white";
 
 document.getElementsByClassName("btn")[0].style.backgroundColor = "blue";
 
+document.querySelector("button").classList.add("invisible");
+
+document.querySelector("btn").classList.remove("invisible");
+
+document.querySelector("btn").classList.toggle("invisible");
+
 document.getElementByTagName("checkbox").checked = "true";
 
 document.querySelector("li a").innerHTML = "I'm First";
@@ -17,6 +23,5 @@ document.querySelector("#list a").style.color = "red";
 document.querySelector("li.item2").innerHTML = "I'm second";
 
 document.querySelector("#list .item3").innerHTML = "I'm Third";
-
 
 document.querySelectorAll("#list2 .item")[1].style.color = "red";
