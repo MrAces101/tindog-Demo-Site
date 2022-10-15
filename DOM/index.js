48eb6f8562ec0653eab20f4998c3b171a89bc40f
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-10-15 16:42:54 -0400

    Selecting HTML elements with JS

--- DOM/index.js
+++ DOM/index.js
@@ -2,6 +2,15 @@ document.querySelector("h1").innerHTML = "Good Bye";
 
 // alert('hello')
 
-heading.style.color = "red";
+document.getElementById("header").style.color = "blue";
 
-document.querySelector("h1").color = "red";
+document.querySelector("li a").innerHTML = "I'm First";
+
+document.querySelector("#list a").style.color = "red";
+
+document.querySelector("li.item2").innerHTML = "I'm second";
+
+document.querySelector("#list .item3").innerHTML = "I'm Third";
+
+
+document.querySelectorAll("#list2 .item")[1].style.color = "red";
