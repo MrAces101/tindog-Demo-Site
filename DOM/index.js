ea6ee198e3f4343e1e2d76fd3e2f26630e06786e
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-10-15 16:13:46 -0400

    external JS example - innerHTML

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,7 @@
-// document.querySelector("h1").innerHTML = "Good Bye";
+document.querySelector("h1").innerHTML = "Good Bye";
+
+// alert('hello')
+
+heading.style.color = "red";
+
+document.querySelector("h1").color = "red";
